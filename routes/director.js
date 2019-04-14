0be6ea1d2d5479263e028e78c58a3ff032b1e1b9
Muhammed Özcan <mmozcan4@gmail.com>
2019-04-14 12:50:31 +0300

    /api/directors GET added

--- routes/director.js
+++ routes/director.js
@@ -9,11 +9,58 @@ router.post('/', (req, res, next) => {
   const director = new Director(req.body);
   const promise = director.save();
 
-  promise.then((data) =>{
+  promise.then((data) => {
     res.json(data);
   }).catch((err) => {
-    res.json(data);
+    res.json(err);
   })
+});
+
+router.get('/', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+
+  ]);
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
